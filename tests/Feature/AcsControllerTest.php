017d794c5962b433990959b249c9997855ae7c5a
Melon Bao <melon.bao@macbook.dev>
2018-12-03 21:19:51 +0800

    [test] change the intergration test for the chaning of soap facade

--- tests/Feature/AcsControllerTest.php
+++ tests/Feature/AcsControllerTest.php
@@ -2,10 +2,9 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Support\Facades\App;
+use App\Models\Facades\SoapFacade;
+use Illuminate\Support\Facades\File;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use \Mockery as m;
 
 class AcsControllerTest extends TestCase
@@ -18,37 +17,42 @@ class AcsControllerTest extends TestCase
     public function setUp()
     {
         parent::setUp();
+        SoapFacade::shouldReceive('ValidSoap')->andReturn(true);
     }
 
-    public function testCpeLogin()
+    public function testRequestNoAuth()
     {
-        $mock = m::mock('App\Interfaces\ICpeContract');
-        $this->app->instance('App\Interfaces\ICpeContract',$mock);
-
-        $mock->shouldReceive('cpeBlankUserAuth')->once()->andReturn(false);
-        $mock->shouldReceive('cpeSavedUserAuth')->once()->andReturn(false);
-        $response = $this->post('http://xacs/tr069');
+        //$this->call($method, $uri, $parameters, $files, $server, $content);
+        $response=$this->call('POST', 'http://xacs/tr069', [], [], [],
+            [], 'xml');
         $response->assertStatus(401);
-
-        $mock->shouldReceive('cpeBlankUserAuth')->once()->andReturn(true);
-        $response = $this->post('http://xacs/tr069');
-        $response->assertStatus(200);
-
-
-        $mock->shouldReceive('cpeBlankUserAuth')->once()->andReturn(false);
-        $mock->shouldReceive('cpeSavedUserAuth')->once()->andReturn(true);
-        $response = $this->post('http://xacs/tr069');
-        $response->assertStatus(200);
-
     }
 
-    public function testInvalid()
+    public function testRequestEmptyUser()
     {
-
+        $response = $this->call('POST','http://xacs/tr069',[],[],[],
+            ['HTTP_AUTHORIZATION'=>'Basic ' . base64_encode(':'),'PHP_AUTH_USER'=>'','PHP_AUTH_PW'=>''],'xml');
+        $response->assertStatus(200);
     }
+
+    public function testRequestSavedUser()
+    {
+        $mock_cpe = m::mock('App\Interfaces\ICpeContract');
+        $mock_cpe->shouldReceive('cpeBlankUserAuth')->once()->andReturn(false);
+        $mock_cpe->shouldReceive('cpeSavedUserAuth')->once()->andReturn(true);
+        $this->app->instance('App\Interfaces\ICpeContract',$mock_cpe);
+
+        $response = $this->call('POST','http://xacs/tr069',[],[],[],
+            ['HTTP_AUTHORIZATION'=>'Basic ' . base64_encode('test:test'),
+             'PHP_AUTH_USER'=>'test',
+             'PHP_AUTH_PW'=>'test'], 'xml');
+        $response->assertStatus(200);
+    }
+
     public function tearDown()
     {
-        parent::tearDown();
         m::close();
+        parent::tearDown();
+
     }
 }
